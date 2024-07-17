44b552847f15928591e43ffc8fef199ad21a1ab3
ShuichiroYoneoka <yoneoka.s.1112@gmail.com>
2024-07-18 01:42:41 +0900

    Add files via upload

---
+++ src/models/evflownet_final.py
@@ -0,0 +1,113 @@
+import torch
+from torch import nn
+from src.models.base import *
+from typing import Dict, Any
+import torch.nn.functional as F
+
+_BASE_CHANNELS = 64
+
+class EVFlowNet(nn.Module):
+    def __init__(self, args):
+        super(EVFlowNet,self).__init__()
+        self._args = args
+
+        self.encoder1 = general_conv2d(in_channels = 4, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder2 = general_conv2d(in_channels = _BASE_CHANNELS, out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder3 = general_conv2d(in_channels = 2*_BASE_CHANNELS, out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder4 = general_conv2d(in_channels = 4*_BASE_CHANNELS, out_channels=8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+
+        self.resnet_block = nn.Sequential(*[build_resnet_block(8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm) for i in range(2)])
+
+        self.decoder1 = upsample_conv2d_and_predict_flow(in_channels=16*_BASE_CHANNELS,
+                        out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+
+        self.decoder2 = upsample_conv2d_and_predict_flow(in_channels=8*_BASE_CHANNELS+2,
+                        out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+
+        self.decoder3 = upsample_conv2d_and_predict_flow(in_channels=4*_BASE_CHANNELS+2,
+                        out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+
+        self.decoder4 = upsample_conv2d_and_predict_flow(in_channels=2*_BASE_CHANNELS+2,
+                        out_channels=int(_BASE_CHANNELS/2), do_batch_norm=not self._args.no_batch_norm)
+
+    def forward(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        # encoder
+        skip_connections = {}
+        inputs = self.encoder1(inputs)
+        skip_connections['skip0'] = inputs.clone()
+        inputs = self.encoder2(inputs)
+        skip_connections['skip1'] = inputs.clone()
+        inputs = self.encoder3(inputs)
+        skip_connections['skip2'] = inputs.clone()
+        inputs = self.encoder4(inputs)
+        skip_connections['skip3'] = inputs.clone()
+
+        # transition
+        inputs = self.resnet_block(inputs)
+
+        # decoder
+        flow_dict = {}
+        inputs = torch.cat([inputs, skip_connections['skip3']], dim=1)
+        inputs, flow0 = self.decoder1(inputs)
+        flow_dict['flow0'] = flow0.clone()
+
+        inputs = torch.cat([inputs, skip_connections['skip2']], dim=1)
+        inputs, flow1 = self.decoder2(inputs)
+        flow_dict['flow1'] = flow1.clone()
+
+        inputs = torch.cat([inputs, skip_connections['skip1']], dim=1)
+        inputs, flow2 = self.decoder3(inputs)
+        flow_dict['flow2'] = flow2.clone()
+
+        inputs = torch.cat([inputs, skip_connections['skip0']], dim=1)
+        inputs, flow3 = self.decoder4(inputs)
+        flow_dict['flow3'] = flow3.clone()
+
+
+        target_size = flow3.size()[2:]  #高さと幅を取得 #[8, 2, 480, 640]
+
+        # 各テンソルをリサイズ
+        flow1_resized = F.interpolate(flow1, size=target_size, mode='bilinear', align_corners=False)
+        flow2_resized = F.interpolate(flow2, size=target_size, mode='bilinear', align_corners=False)
+        flow0_resized = F.interpolate(flow0, size=target_size, mode='bilinear', align_corners=False)
+
+        flow = torch.cat((flow0_resized, flow1_resized, flow2_resized, flow3), dim=1) #[8, 8, 480, 640]
+        if flow.size() == torch.Size([8, 8, 480, 640]):
+            flow = flow.view(8, 4, 2, 480, 640) #平均を取ってチャネル数を2に減らした
+            flow = flow.mean(dim=1)
+
+        else:
+            pass
+
+        return flow #flowが[8, 8, 480, 640]のsizeでない可能性もある
+        
+
+# if __name__ == "__main__":
+#     from config import configs
+#     import time
+#     from data_loader import EventData
+#     '''
+#     args = configs()
+#     model = EVFlowNet(args).cuda()
+#     input_ = torch.rand(8,4,256,256).cuda()
+#     a = time.time()
+#     output = model(input_)
+#     b = time.time()
+#     print(b-a)
+#     print(output['flow0'].shape, output['flow1'].shape, output['flow2'].shape, output['flow3'].shape)
+#     #print(model.state_dict().keys())
+#     #print(model)
+#     '''
+#     import numpy as np
+#     args = configs()
+#     model = EVFlowNet(args).cuda()
+#     EventDataset = EventData(args.data_path, 'train')
+#     EventDataLoader = torch.utils.data.DataLoader(dataset=EventDataset, batch_size=args.batch_size, shuffle=True)
+#     #model = nn.DataParallel(model)
+#     #model.load_state_dict(torch.load(args.load_path+'/model18'))
+#     for input_, _, _, _ in EventDataLoader:
+#         input_ = input_.cuda()
+#         a = time.time()
+#         (model(input_))
+#         b = time.time()
+#         print(b-a)
